848f9487dee17a48b853f0917493b13feb68c36d
Shohel Rana <cse_shohel@yahoo.com>
2017-04-26 22:39:53 +0600

    Design employee form

--- client/app/hrm/components/employee.component.ts
+++ client/app/hrm/components/employee.component.ts
@@ -7,13 +7,40 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'employee',
-  templateUrl: '../templates/employee.component.html',  
+  templateUrl: '../templates/employee.component.html',
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
-  
-  ngOnInit() {
+  //validations messages settings
+  addEmployeeForm: FormGroup;
+  firstName = new FormControl('', Validators.required);
+  lastName = new FormControl('', Validators.required);
+  fullName = new FormControl('', Validators.required);
+  title = new FormControl('', Validators.required);
+  dateOfBirth = new FormControl('', Validators.required);
+  address = new FormControl('', Validators.required);
+  bio = new FormControl('', Validators.required);
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    //build the form
+    this.addEmployeeForm = this.formBuilder.group({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      fullName: this.fullName,
+      title: this.title,
+      dateOfBirth: this.dateOfBirth,
+      address: this.address,
+      bio: this.bio
+    });
+  }
+
+  //add employee
+  addEmployee(): void {
 
   }
+
+
 }
