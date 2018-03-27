eae62d5c3e2a34deb9d90df03b08a8c246f0f7b2
Sohel <Sohel@NEXT_IT.com>
2018-03-27 17:34:11 +0600

    fix bug of npm run prod

--- server/app.ts
+++ server/app.ts
@@ -31,7 +31,7 @@ db.once('open', () => {
   });
 
   app.listen(app.get('port'), () => {
-    console.log('Angular Full Stack listening on port ' + app.get('port'));
+    console.log('Mean Stack listening on port ' + app.get('port'));
   });
 
 });
